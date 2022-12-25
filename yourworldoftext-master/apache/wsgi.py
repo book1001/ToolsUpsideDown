e735fe687602146c5f83260ecd5060d7a5130a74
Halim <61380938+book1001@users.noreply.github.com>
2022-12-24 22:17:42 -0500

    Delete yourworldoftext-master directory

--- yourworldoftext-master/apache/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-sys.path.append('/var/www/yourworld/')
-sys.path.append('/var/www/')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
